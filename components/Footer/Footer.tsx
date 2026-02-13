9c26e1c7c5135672dc5631dfdcce7b755fa365ae
Skyler688 <skylergit86@gmail.com>
2026-02-12 19:02:08 -0700

    basic project layout

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import source from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={source.footer}>
+      <p>footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Navbar = () => {
+  return (
+    <header className={styles.header}>
+      <p>Header</p>
+    </header>
+  );
+};
+
+export default Navbar;
